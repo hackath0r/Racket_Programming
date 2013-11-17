09d4047bcfc0edceefec3f1a93fc3b03b3f07457
planimos <prashant.bhrgv@gmail.com>
2013-11-17 05:43:48 +0530

    Added intro to language files.

---
+++ racket_intro.rkt
@@ -0,0 +1,11 @@
+; always make this the first (non-comment, non-blank)
+; line of your file 
+#lang racket 
+
+; not needed here, but a workaround so we could write tests
+; in a second file see getting-started-with-Racket 
+; instructions for more explantion
+(provide (all-defined-out))
+
+;basic definitions
+(define s "hello") ; this is another comment
